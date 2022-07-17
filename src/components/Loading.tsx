30395173f0d63c54046f1b225b399161bbd870f0
Hüseyin Ensari <hsynensari@gmail.com>
2022-07-17 17:04:12 +0300

    Add Loading Compenent

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className='w-full flex justify-center items-center h-96'>
+            <div className='w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-gray-700'></div>
+        </div>
+    );
+};
+
+export default Loading;
