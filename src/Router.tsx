1d31eb4cb1fd0f861266c52eef5d1be4f11138c7
Hüseyin Ensari <hsynensari@gmail.com>
2022-07-17 22:59:08 +0300

    Add Error Page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage, ProductDetail, AddProduct } from './pages';
+import { HomePage, ProductDetail, AddProduct, ErrorPage } from './pages';
 
 function App() {
     return (
@@ -8,6 +8,7 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='/product/:id' element={<ProductDetail />} />
                 <Route path='/add' element={<AddProduct />} />
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </BrowserRouter>
     );
