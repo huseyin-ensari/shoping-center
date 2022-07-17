f3398d44d3b90c40f406b4f6c551d4824a391c96
Hüseyin Ensari <hsynensari@gmail.com>
2022-07-17 17:02:06 +0300

    Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import imageNotFoundPlaceHolder from './imageNotFound.webp';
+
+export { imageNotFoundPlaceHolder };
+// svgs
+export { default as SocialMediaIcons } from './svg/SocialMediaIcons';
+export { default as StarIcon } from './svg/StarIcon';
+export { default as LikeIcon } from './svg/LikeIcon';
